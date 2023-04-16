441c83676dace0ac9e45ed8db418fa94d520af9f
VictrCruz312 <motoggb979@gmail.com>
2023-04-16 15:13:05 -0300

    fix: trocando imagem da seção 7

--- src/components/sections/section7/index.tsx
+++ src/components/sections/section7/index.tsx
@@ -4,14 +4,13 @@ import image1 from "../../../assets/section7/image1.png";
 import image2 from "../../../assets/section7/image2.png";
 import image3 from "../../../assets/section7/image3.png";
 import image4 from "../../../assets/section7/image4.png";
-import teste from "../../../assets/blogImages/exercise-2.png";
 
 const Section7 = () => {
     return (
         <Section7Styled>
             <h2 className="titleSection">Objetivos</h2>
             <div className="containerObjetivos">
-                <CardObjective image={teste} title="Ganho de Massa" />
+                <CardObjective image={image1} title="Ganho de Massa" />
                 <CardObjective image={image2} title="Energia" />
                 <CardObjective image={image3} title="Recuperação Muscular" />
                 <CardObjective image={image4} title="Emagrecimento" />
